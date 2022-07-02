cae870d2160ff86fe3b825308e7a6a8c675741e6
Alexander Zaytsev <me@axv.email>
2022-07-02 11:18:15 +0300

    rename django project

--- daf/daf/urls.py
+++ daf/daf/urls.py
@@ -1,4 +1,4 @@
-"""audio URL Configuration
+"""daf URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
