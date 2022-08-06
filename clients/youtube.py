65f4b2dd2db7e509279124d60a47f87b26b4a813
Alexander Zaytsev <me@axv.email>
2022-08-06 13:59:27 +0300

    client json response handle

--- clients/youtube.py
+++ clients/youtube.py
@@ -93,7 +93,10 @@ class YouTubeEpisodeHandler:
                 files['image'] = (self.image.name, self.image)
             resp = requests.post(upload_url, data=data, files=files, auth=auth)
 
-        response = resp.json()
+        json_statuses = {200, 400}
+        response = resp.json() if resp.status_code in json_statuses else resp.text
+
+        print(response)
         if resp.status_code != 200:
             print(f'status={resp.status_code}\n{response}', file=sys.stderr)
             sys.exit(1)
